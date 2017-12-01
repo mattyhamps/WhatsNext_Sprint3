fbd2647468f942fbf11d5b3c0aecb0aec0e1598e
Laura <Laura@laura>
2017-12-01 01:12:49 -0500

    all the controllers are combined now

--- src/WhatsNext_Sprint3/MainMenuController.java
+++ src/WhatsNext_Sprint3/MainMenuController.java
@@ -5,7 +5,13 @@
  */
 package WhatsNext_Sprint3;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
 /**
@@ -13,6 +19,88 @@ import javafx.fxml.FXML;
  * @author mattb
  */
 public class MainMenuController {
+    @FXML private AnchorPane pane;
+    
+    @FXML protected void handleProfileButtonAction(ActionEvent event) {
+        
+        
+        try{
+            Stage stage1 = (Stage)pane.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CreateProfile.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            stage1.setWidth(600);
+            Scene scene = new Scene(root, 600, 400);
+            stage1.setScene(scene);
+
+            stage1.show();
+        }catch(Exception e){
+
+        }
+    }
+    @FXML protected void handleSearchMovieButtonAction(ActionEvent event) {
+        
+        
+        try{
+            Stage stage1 = (Stage)pane.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Search_Movie.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            stage1.setWidth(400);
+            Scene scene = new Scene(root, 400, 400);
+            stage1.setScene(scene);
+
+            stage1.show();
+        }catch(Exception e){
+
+        }
+    }
+    @FXML protected void handleSearchBookButtonAction(ActionEvent event) {
+        
+        
+        try{
+            Stage stage1 = (Stage)pane.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Search_Books.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            stage1.setWidth(400);
+            Scene scene = new Scene(root, 400, 400);
+            stage1.setScene(scene);
+
+            stage1.show();
+        }catch(Exception e){
+
+        }
+    }
+    @FXML protected void handleLogOutButtonAction(ActionEvent event) {
+        
+        
+        try{
+            Stage stage1 = (Stage)pane.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Login.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            stage1.setWidth(400);
+            Scene scene = new Scene(root, 400, 300);
+            stage1.setScene(scene);
+
+            stage1.show();
+        }catch(Exception e){
+
+        }
+    }
+    @FXML protected void handleHistoryRecommendationAction(ActionEvent event) {
+        
+        try{
+            Stage stage1 = (Stage)pane.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("SearchResult.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            stage1.setWidth(400);
+            Scene scene = new Scene(root, 400, 300);
+            stage1.setScene(scene);
+
+            stage1.show();
+        }catch(Exception e){
+
+        }
+    }
+        
     
     
 }
